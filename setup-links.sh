825e2123d15bcbed3702a2372314cf78e7d0cbe6
John Quagliata <john.quagliata@gmail.com>
2023-11-25 10:15:15 -0600

    putting tmux in xdg config

--- setup-links.sh
+++ setup-links.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-#ln -s $(pwd)/.vimrc ${HOME}/.vimrc
-#ln -s $(pwd)/.ideavimrc ${HOME}/.ideavimrc
-#ln -s $(pwd)/.zshrc ${HOME}/.zshrc
-
 mkdir -p ${HOME}/.config
 
-ln -s $(pwd)/.config/nvim ${HOME}/.config/nvim
-ln -s $(pwd)/.config/fish ${HOME}/.config/fish
-ln -s $(pwd)/.config/starship.toml ${HOME}/.config/starship.toml
+ln -s $(pwd)/.config/nvim $XDG_CONFIG_HOME/nvim
+ln -s $(pwd)/.config/fish $XDG_CONFIG_HOME/fish
+ln -s $(pwd)/.config/starship.toml $HOME/.config/starship.toml
+
+ln -s $(pwd)/.config/tmux/tmux.conf.local $XDG_CONFIG_HOME/tmux/tmux.conf.local
 
 # TODO move this to the .config folder
 #mkdir -p ${HOME}/.config/karabiner/
